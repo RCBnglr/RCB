de036dcbba259e29f5ca2ff6dc2e1cd915d5ced6
HANUMANTA NETREKAR <lohithanumanta.netrekar.ext@siemens.com>
2018-12-19 14:44:19 +0530

    Project started

---
+++ src/app/userhome/userhome.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-userhome',
+  templateUrl: './userhome.component.html',
+  styleUrls: ['./userhome.component.css']
+})
+export class UserhomeComponent implements OnInit {
+
+  constructor(private _router:Router) { }
+
+  ngOnInit() {
+  }
+
+  moveToLogin(){
+    this._router.navigate(['/login']);
+  }
+}
