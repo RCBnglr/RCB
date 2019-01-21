af24a77de150d625dcf328772509d64763789967
LOHIT HANUMANTA NETREKAR <lohithanumanta.netrekar.ext@siemens.com>
2019-01-21 11:51:45 +0530

    dashboard

--- src/app/userhome/userhome.component.ts
+++ src/app/userhome/userhome.component.ts
@@ -39,35 +39,35 @@ export class UserhomeComponent implements OnInit {
 
   ngOnInit() {
   }
-  filterSelection(all:any)
-      filterSelection(c) {
-        var x, i;
-        x = document.getElementsByClassName("filterDiv");
-        if (c == "all") c = "";
-        for (i = 0; i < x.length; i++) {
-          this.w3RemoveClass(x[i], "show");
-          if (x[i].className.indexOf(c) > -1) this.w3AddClass(x[i], "show");
-        }
-      }
-      w3RemoveClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-          while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);     
-          }
-        }
-        element.className = arr1.join(" ");
-      }
-      w3AddClass(element, name) {
-        var i, arr1, arr2;
-        arr1 = element.className.split(" ");
-        arr2 = name.split(" ");
-        for (i = 0; i < arr2.length; i++) {
-          if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-        }
-      }
+  // filterSelection(all:any)
+  //     filterSelection(c) {
+  //       var x, i;
+  //       x = document.getElementsByClassName("filterDiv");
+  //       if (c == "all") c = "";
+  //       for (i = 0; i < x.length; i++) {
+  //         this.w3RemoveClass(x[i], "show");
+  //         if (x[i].className.indexOf(c) > -1) this.w3AddClass(x[i], "show");
+  //       }
+  //     }
+  //     w3RemoveClass(element, name) {
+  //       var i, arr1, arr2;
+  //       arr1 = element.className.split(" ");
+  //       arr2 = name.split(" ");
+  //       for (i = 0; i < arr2.length; i++) {
+  //         while (arr1.indexOf(arr2[i]) > -1) {
+  //           arr1.splice(arr1.indexOf(arr2[i]), 1);     
+  //         }
+  //       }
+  //       element.className = arr1.join(" ");
+  //     }
+  //     w3AddClass(element, name) {
+  //       var i, arr1, arr2;
+  //       arr1 = element.className.split(" ");
+  //       arr2 = name.split(" ");
+  //       for (i = 0; i < arr2.length; i++) {
+  //         if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  //       }
+  //     }
       
             
       // <!-- // // Add active class to the current button (highlight it)
