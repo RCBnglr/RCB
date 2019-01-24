5f9b9d5e889603b66b50a558a266b9a5229af3a5
LOHIT HANUMANTA NETREKAR <lohithanumanta.netrekar.ext@siemens.com>
2019-01-24 14:11:15 +0530

    Dynamic data Update-lohit

--- src/app/userhome/userhome.component.ts
+++ src/app/userhome/userhome.component.ts
@@ -97,12 +97,6 @@ export class UserhomeComponent implements OnInit {
   // moveToLogin(){
   //   this._router.navigate(['/login']);
   // }
-  public portfolio=[
-    {name:'Project 1', status:'ongoing'},         
-    {name:'Project 2', status:'ongoing'},
-    {name:'Project 3', status:'ongoing'},
-    {name:'Project 4', status:'ongoing'},
-    {name:'Project 5', status:'ongoing'},
-    {name:'Project 6', status:'Completed'}];
+ 
 }
 
